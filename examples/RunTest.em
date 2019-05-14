f2ca161c9c9ba713db881a1c81510fd84a75738c
Andrew Luckett <andyluckett9@gmail.com>
2019-05-14 16:48:41 +0100

    Add the ability to run other Embungo programs

---
+++ examples/RunTest.em
@@ -0,0 +1,18 @@
+//Input your own start acc value
+VPrint
+Store 1
+
+Set 32
+APrint
+
+Load 1
+Run math/sqrt.em
+VPrint
+
+Set 10
+APrint
+
+Run HelloWorld1.em
+Run HelloWorld2.em
+
+Done
--- vm/EmPyVm.py
+++ vm/EmPyVm.py
@@ -2,38 +2,34 @@
 
 class Commands:
     class Jumps:
-        def jump(a):
-            global headLocation
+        def jump(parent,a):
             if type(a) != int:
                 a = int(a)
-            headLocation = a-1
+            parent.headLocation = a-1
             return True
 
-        def jumpPos(a):
-            global acc, headLocation
+        def jumpPos(parent,a):
             if type(a) != int:
                 a = int(a)
-            if acc > 0:
-                headLocation = a-1
+            if parent.acc > 0:
+                parent.headLocation = a-1
             return True
         
-        def jumpNeg(a):
-            global acc, headLocation
+        def jumpNeg(parent,a):
             if type(a) != int:
                 a = int(a)
-            if acc < 0:
-                headLocation = a-1
+            if parent.acc < 0:
+                parent.headLocation = a-1
             return True
 
-        def jumpZero(a):
-            global acc, headLocation
+        def jumpZero(parent,a):
             if type(a) != int:
                 a = int(a)
-            if acc == 0:
-                headLocation = a-1
+            if parent.acc == 0:
+                parent.headLocation = a-1
             return True
 
-        def done(a):
+        def done(parent,a):
             return False
 
         commands = {
@@ -45,25 +41,22 @@ class Commands:
         }
 
     class Mem:
-        def load(a):
-            global acc
+        def load(parent,a):
             if type(a) != int:
                 a = int(a)
-            acc = int(getDat(a-1)[0])
+            parent.acc = int(parent.getDat(a-1)[0])
             return True
 
-        def store(a):
-            global acc, tape
+        def store(parent,a):
             if type(a) != int:
                 a = int(a)
-            tape[a-1] = acc
+            parent.tape[a-1] = parent.acc
             return True
 
-        def seta(a):
-            global acc
+        def seta(parent,a):
             if type(a) != int:
                 a = int(a)
-            acc = a
+            parent.acc = a
             return True
 
         commands = {
@@ -73,39 +66,34 @@ class Commands:
         }
 
     class Math:
-        def add(a):
-            global acc
+        def add(parent,a):
             if type(a) != int:
                 a = int(a)
-            acc += int(getDat(a-1)[0])
+            parent.acc += int(parent.getDat(a-1)[0])
             return True
 
-        def sub(a):
-            global acc
+        def sub(parent,a):
             if type(a) != int:
                 a = int(a)
-            acc -= int(getDat(a-1)[0])
+            parent.acc -= int(parent.getDat(a-1)[0])
             return True
 
-        def mult(a):
-            global acc
+        def mult(parent,a):
             if type(a) != int:
                 a = int(a)
-            acc *= int(getDat(a-1)[0])
+            parent.acc *= int(parent.getDat(a-1)[0])
             return True
 
-        def div(a):
-            global acc
+        def div(parent,a):
             if type(a) != int:
                 a = int(a)
-            acc = acc // int(getDat(a-1)[0])
+            parent.acc = parent.acc // int(parent.getDat(a-1)[0])
             return True
 
-        def mod(a):
-            global acc
+        def mod(parent,a):
             if type(a) != int:
                 a = int(a)
-            acc = acc % int(getDat(a-1)[0])
+            parent.acc = parent.acc % int(parent.getDat(a-1)[0])
             return True
 
         commands = {
@@ -117,87 +105,87 @@ class Commands:
         }
 
     class Io:
-        def aprint(a):
-            global acc
-            print(chr(acc),end="")
+        def aprint(parent,a):
+            print(chr(parent.acc),end="")
             return True
 
-        def vprint(a):
-            global acc
-            print(acc, end="")
+        def vprint(parent,a):
+            print(parent.acc, end="")
             return True
 
-        def inp(a):
-            global acc
+        def inp(parent,a):
             inpu = input()
             try:
                 inpu = int(inpu)
             except:
                 inpu = ord(inpu[0])
-            acc = inpu
+            parent.acc = inpu
             return True
 
+        def run(parent,a):
+            b = Vm(parent.fileLocation.rsplit("/",1)[0] + "/" + a)
+            b.acc = parent.acc
+            b.runTape()
+            parent.acc = b.acc
+            return True
+            
         commands = {
             "aprint":aprint,
             "vprint":vprint,
-            "input":inp
+            "input":inp,
+            "run":run
         }
 
 #######
+class Vm:
+    def __init__(this, location):
+        this.tape = []
+        with open(location) as f:
+            for line in f:
+                this.tape.append(line.replace("\n",""))
+        this.fileLocation = location
+        this.headLocation = 0
+        this.acc = 0
+        
+        this.commands = {}
+        this.commands.update(Commands.Jumps.commands)
+        this.commands.update(Commands.Mem.commands)
+        this.commands.update(Commands.Io.commands)
+        this.commands.update(Commands.Math.commands)
 
-headLocation = 0
-acc = 0
-tape = []
-commands = {}
+    def runTape(this):
+        while(True):
+            command = this.getDat(this.headLocation)
+            #print(command)
+            this.headLocation += 1
+            if this.runCommand(command) == False:
+                return
 
-commands.update(Commands.Jumps.commands)
-commands.update(Commands.Mem.commands)
-commands.update(Commands.Io.commands)
-commands.update(Commands.Math.commands)
+    def getDat(this,line):
+        if type(this.tape[line]) == int:
+            return [this.tape[line]] #Expected to return an array
+        
+        command = this.tape[line].split("//")[0].split(" ")
+        while command.count(""):
+            command.remove("")
+        return command
 
-def loadTape(location):
-    global tape
-    with open(location) as f:
-        for line in f:
-            tape.append(line.replace("\n",""))
+    def runCommand(this,command):
+        if len(command) == 0:
+            #print(">> ERR: Tried to execute an empty line") 
+            return True
+        
+        com = command[0].lower()
+        func = this.commands.get(com)
 
-def runTape():
-    global headLocation
+        if func == None:
+            print(">> ERR: Tried to execute an invalid command :",com,this.fileLocation)
+            return False
 
-    while(True):
-        command = getDat(headLocation)
-        #print(command)
-        headLocation += 1
-        if runCommand(command) == False:
-            return
+        if len(command) == 1:
+            return func(this,None)
 
-def getDat(line):
-    global tape
-    if type(tape[line]) == int:
-        return [tape[line]] #Expected to return an array
-    
-    command = tape[line].split("//")[0].split(" ")
-    while command.count(""):
-        command.remove("")
-    return command
-
-def runCommand(command):
-    global acc,headLocation
-    if len(command) == 0:
-        #print(">> ERR: Tried to execute an empty line") 
-        return True
-    
-    com = command[0].lower()
-    func = commands.get(com)
-
-    if func == None:
-        print(">> ERR: Tried to execute an invalid command :",com)
-        return False
-
-    if len(command) == 1:
-        return func(None)
-
-    return func(command[1])
+        return func(this,command[1])
 
 #######
 
@@ -205,12 +193,12 @@ if __name__ == "__main__":
     import sys
     
     try:
-        loadTape(sys.argv[1])
+        a = Vm(sys.argv[1])
     except:
         print(">> ERR: Unable to load program")
         exit()
 
     if(len(sys.argv) >= 3):
-        acc = int(sys.argv[2])
+        a.acc = int(sys.argv[2])
 
-    runTape()
+    a.runTape()
